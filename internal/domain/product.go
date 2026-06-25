2999fbd4c8ca24a33773f30f87da799dc856ae54
Pinya Aungkanarungrut <88120284+DEARDOYY@users.noreply.github.com>
2026-06-25 20:42:56 +0700

    Create product.go

---
+++ internal/domain/product.go
@@ -0,0 +1,20 @@
+package domain
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Product struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Name        string             `bson:"name" json:"name"`
+	Description string             `bson:"description" json:"description"`
+	Price       float64            `bson:"price" json:"price"`
+	Stock       int                `bson:"stock" json:"stock"`
+	CategoryID  primitive.ObjectID `bson:"category_id" json:"category_id"`
+	Images      []string           `bson:"images" json:"images"`
+	IsActive    bool               `bson:"is_active" json:"is_active"`
+	CreatedAt   time.Time          `bson:"created_at" json:"created_at"`
+	UpdatedAt   time.Time          `bson:"updated_at" json:"updated_at"`
+}
