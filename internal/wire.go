820102365de2641d911d8a13ec84285c336828ae
Pinya Aungkanarungrut <88120284+DEARDOYY@users.noreply.github.com>
2026-07-03 00:25:24 +0700

    Update wire.go

--- internal/wire.go
+++ internal/wire.go
@@ -1,1 +1,27 @@
 package internal
+
+import (
+	"ecommerce-api/internal/handler"
+	"ecommerce-api/internal/repository"
+	"ecommerce-api/internal/usecase"
+	"ecommerce-api/pkg/database"
+	"os"
+)
+
+type Handlers struct {
+	Auth *handler.AuthHandler
+}
+
+func InitHandlers() *Handlers {
+
+	userCollection := database.GetCollection(os.Getenv("MONGO_DB_NAME"), "users")
+
+	userRepo := repository.NewUserRepository(userCollection)
+
+	authUsecase := usecase.NewAuthUsecase(userRepo)
+	authHandler := handler.NewAuthHandler(authUsecase)
+
+	return &Handlers{
+		Auth: authHandler,
+	}
+}
