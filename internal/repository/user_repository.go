038aaeb5e68a0f93c9a68de892514537427be270
Pinya Aungkanarungrut <88120284+DEARDOYY@users.noreply.github.com>
2026-08-25 21:07:09 +0700

    Update user_repository.go

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"ecommerce-api/internal/domain"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type UserRepository interface {
 	Create(ctx context.Context, user *domain.User) error
+	FindByEmail(ctx context.Context, email string) (*domain.User, error)
 }
 
 type userRepository struct {
@@ -29,4 +31,13 @@ func (r *userRepository) Create(ctx context.Context, user *domain.User) error {
 	user.ID = result.InsertedID.(primitive.ObjectID)
 
 	return nil
+}
+
+func (r *userRepository) FindByEmail(ctx context.Context, email string) (*domain.User, error) {
+	var user domain.User
+	err := r.collection.FindOne(ctx, bson.M{"email": email}).Decode(&user)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
