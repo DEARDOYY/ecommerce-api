46acdef59c1cce300bdce4c8bedd147372e91aa9
Pinya Aungkanarungrut <88120284+DEARDOYY@users.noreply.github.com>
2026-09-01 10:36:23 +0700

    Update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"log"
+
+	"ecommerce-api/internal"
+	"ecommerce-api/internal/routes"
+	"ecommerce-api/pkg/database"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, using system env")
+	}
+
+	client := database.ConnectDB()
+	defer client.Disconnect(nil)
+
+	h := internal.InitHandlers()
+
+	r := gin.Default()
+	routes.SetupRoutes(r, h)
+	r.Run(":8080")
+
+}
