68fd27c3b5986728a0e05d8bc5391f69241ad573
Pinya Aungkanarungrut <88120284+DEARDOYY@users.noreply.github.com>
2026-09-13 22:55:54 +0700

    Create auth.go

---
+++ internal/routes/auth.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"ecommerce-api/internal/handler"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthRoute(rg *gin.RouterGroup, h *handler.AuthHandler) {
+	user := rg.Group("/auth")
+	{
+		user.POST("/register", h.Register)
+		user.POST("/login", h.Login)
+	}
+}
